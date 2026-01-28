ee809e212a128a8f704197eee13c88a97d9d12fa
Renato Matos <renato.matos@cgi.com>
2026-01-28 14:08:48 +0000

    adding tests menu

--- py-from-zero-to-hero-13/src/demo/main.py
+++ py-from-zero-to-hero-13/src/demo/main.py
@@ -1,2 +1,5 @@
-print("building an extension")
-pint("Simple pythong code")
+def register_user(name, age=18, *skills, active=True, **metadata):
+    print(name, age, skills, active, metadata)
+
+register_user("Renato")
+register_user("Renato", 35, "Python", "C#", active=False, country="Portugal")
