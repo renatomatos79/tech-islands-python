a17b53cd5b455a6c4ae6b63145255351d743bde3
Renato Matos <renato.matos@cgi.com>
2026-02-02 14:59:10 +0000

    adding doc

--- py-from-zero-to-hero-14/playground/main_http.py
+++ py-from-zero-to-hero-14/playground/main_http.py
@@ -8,6 +8,10 @@ mcp = FastMCP("random_name")
 
 @mcp.tool
 def get_random_name(names: list = []) -> str:
+    """Gets a random peoples names. The names are stored in a local array
+    args:
+       names:the user can pass in a list of names to choose from, or it will default to a predefined list.
+    """
     if len(names)  > 0:
         return choice(names)
     return choice(["Alice","Bob","Charlie","Diana","Eve","Frank","Grace","Hank","Ivy","Jack"])
